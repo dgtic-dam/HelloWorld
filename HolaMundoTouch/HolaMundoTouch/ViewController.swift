c5b965e4bcc3397a32e5734037d49cfd12da7dec
dannas@protonmail.com <infraestructura@iMac-de-Infraestructura-91.local>
2019-06-07 19:31:52 -0500

    First commit, swift project

---
+++ HolaMundoTouch/HolaMundoTouch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolaMundoTouch
+//
+//  Created by Infraestructura on 6/7/19.
+//  Copyright © 2019 Dan Ros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
