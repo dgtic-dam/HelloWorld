26227941cfe6960f51a2ac92bf74b934bd5e6e34
DanJRM <dannas@protonmail.com>
2019-06-14 20:09:14 -0500

    User interface updated

--- Actions/Actions/ViewController.swift
+++ Actions/Actions/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ResizeY: NSLayoutConstraint!
+    
+    @IBAction func btnEnter(_ sender: Any) {
+        var mensaje = ""
+        let correo = txtCorreo.text!
+        if correo.isEmpty{
+            mensaje = "Escribe tu correo"
+        }
+        if mensaje != ""{
+            let actionController = UIAlertController(title: "Error", message: mensaje, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            actionController.addAction(alertAction)
+            self.present(actionController, animated: true, completion: nil)
+        }
+        else{
+            //Navega a la siguiente vista
+        }
+    }
+    
+    @IBOutlet weak var txtCorreo: UITextField!
+    
+    @IBAction func TapInView(_ sender: Any) {
+        print("tap")
+        txtCorreo.resignFirstResponder()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(subeTeclado(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //TODO suscribirse a la notificacion keybordWillHideNotification invocando un metodo que se llame "bajaTeclado"
+        NotificationCenter.default.addObserver(self, selector: #selector(bajaTeclado(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+      /*  NotificationCenter.default.removeObserver(self)*/
+    }
+    
+    @objc func subeTeclado(_ notificacion:Notification){
+        print("sube Teclado")
+        ResizeY.constant = -50.0
+    }
+    
+    @objc func bajaTeclado(_ notificacion:Notification){
+        print("baja Teclado")
+        ResizeY.constant = 0.0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       let tapGestureRecognizer = UITapGestureRecognizer(target: self ,action:#selector(TapInView(_:)))
+        
+        self.view.addGestureRecognizer(tapGestureRecognizer)
     }
 
 
