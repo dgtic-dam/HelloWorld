d3ebf484caf740aaa7820cd0fd3ca5471c0481fd
DanJRM <dannas@protonmail.com>
2019-06-21 20:57:21 -0500

    Hamburguer project updated, login form was added

---
+++ HamburguerMenu/HamburguerMenu/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  HamburguerMenu
+//
+//  Created by Infraestructura on 6/21/19.
+//  Copyright © 2019 Dan Ros. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var subStack1: UIStackView!
+    
+    @IBOutlet weak var subStack2: UIStackView!
+    
+    @IBOutlet weak var subStack3: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
