bf8a42f1a4ffdbeb59b36648a91dcba1eaf42791
DanJRM <dannas@protonmail.com>
2019-06-28 17:34:05 -0500

    Class added, NSObject

--- Estados/Estados/DataSource.swift
+++ Estados/Estados/DataSource.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class DataSource: NSObject {
-
+    static func getJsonArray() -> [[String:Any?]] {
+        // 1) Tener el "string" de la URL
+        // Para archivo local
+        // guard let a = 2, let b = 3
+        guard let urlStr = Bundle.main.path(forResource: "estados", ofType: "json") else { return [] }
+        guard let url = URL(string: "file://" + urlStr) else { return [] }
+        // Para archivo remoto
+        // guard let urlStr = "http://archivo.remoto/archivo.json"
+        // let url = URL(string:urlSTR)
+        do {
+            //Data representa un arreglo de bytes
+            let bytes = try Data(contentsOf: url)
+            let temp = try JSONSerialization.jsonObject(with: bytes, options:.allowFragments)
+            return temp as! [[String:Any?]] //forzoso un arreglo de diccionarios
+        }
+        catch{
+            print ("error al abrir el JSON " + error.localizedDescription)
+        }
+        return []
+    }
 }
