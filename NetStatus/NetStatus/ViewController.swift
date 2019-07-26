619e014f73fbc1551bc64c6604f23435611570ee
DanJRM <dannas@protonmail.com>
2019-07-26 17:53:09 -0500

    First commit

--- NetStatus/NetStatus/ViewController.swift
+++ NetStatus/NetStatus/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ac:UIAlertController?
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if let reach = Reachability(hostName: "www.google.com"){
+            let mensaje = ""
+            var confirma = false
             let netStat = reach.currentReachabilityStatus()
             switch (netStat){
             case NotReachable: print ("no hay conexión")
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
             //case ReachableViaWWAN: print ("La conexión es por datos...procedo?")
             default: print ("La conexión es por datos...procedo?")
             }
+            ac = UIAlertController(title: "NetStatus", message: mensaje, preferredStyle: .alert)
+            let aaOK = UIAlertAction(title: "OK", style: default, handler: nil)
         }
     }
     override func viewDidLoad() {
