67c1c028658b040f7b37e38aea0e1e037a9441eb
DanJRM <dannas@protonmail.com>
2019-08-16 18:03:36 -0500

    First commit

---
+++ Protocols/Protocols/MyTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  MyTableViewCell.swift
+//  Protocols
+//
+//  Created by Infraestructura on 8/16/19.
+//  Copyright © 2019 Daniel Rosales. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    
+    private let boton = UIButton()
+    private var bookName = ""
+    
+    //Se sobreescribe init porque no tenemos un XIB queutilice el awakeFromNib
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = UIColor.white
+        // Crear los objetos que va a tener la celda
+        self.boton.setTitle("COMPRAR", for: .normal)
+        self.boton.setTitleColor(UIColor.red, for: .normal)
+        self.boton.backgroundColor = UIColor.yellow
+        self.boton.addTarget(self, action: #selector(botonTouch), for: .touchUpInside)
+        self.addSubview(boton)
+    }
+    
+    @objc func botonTouch(){
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "botonTouch"), object: self, userInfo: ["book":bookName]) // Se genera notificación
+    }
+    
+    func configure(book:String){
+        self.boton.setTitle("COMPRAR \(book)", for: .normal)
+        self.boton.frame = CGRect(x: 10, y: 0, width: self.frame.width - 50, height: self.frame.height)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Protocols/Protocols/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Protocols
+//
+//  Created by Infraestructura on 8/16/19.
+//  Copyright © 2019 Daniel Rosales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let identificador = "myCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identificador, for: indexPath) as! MyTableViewCell
+        cell.configure(book: books[indexPath.row])
+        return cell
+    }
+    
+    //Si el usuario toca la celda
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // _ se omite la etiqueta "tableView" de la firma del método
+        let bookName = books[indexPath.row].replacingOccurrences(of: " ", with: "_")
+        let nomEncode = bookName.addingPercentEncoding(withAllowedCharacters:.urlHostAllowed)
+        let urlStr = "https://google.com/search?q=" + (nomEncode ?? "")
+        UIApplication.shared.open(URL(string: urlStr)!, options: [:], completionHandler: nil)
+        
+    }
+    
+    let books = ["Mujercitas", "Orgullo y Prejuicio", "Bóvedas de Acero", "El viaje del Elefante", "El Padrino", "El Extranjero", "Become an Xcoder"]
+    let tabla = UITableView()
+    
+    
+    @objc func botonTouched(_ noti:Notification){
+        if let userInfo = noti.userInfo{
+            if let bookName = userInfo["book"] as? String{
+                let ac = UIAlertController(title: "Libros", message: "Seguro que deseas comprar" + bookName, preferredStyle: .alert)
+                let a1 = UIAlertAction(title: "SI", style: .default, handler: nil)
+                let a2 = UIAlertAction(title: "NO", style: .destructive, handler: nil)
+                ac.addAction(a1)
+                ac.addAction(a2)
+                self.present(ac, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabla.delegate = self
+        tabla.dataSource = self
+        tabla.register(MyTableViewCell.self, forCellReuseIdentifier: identificador)
+        self.view.addSubview(tabla)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        tabla.frame = self.view.frame.insetBy(dx: 50, dy: 50) //reducido en 50px en horizontal y 50 px en verticla
+    }
+}
+
+
